b6deb48968492cc21aac0a8b546a4a62c483ee7d
mo2hefny <moamensaied1@gmail.com>
2026-06-12 21:19:06 +0300

    chore: add initial files

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026 CoverIt Labs. All Rights Reserved.
+// Proprietary and confidential. Unauthorized use is strictly prohibited.
+// See LICENSE file in the project root for full license information.
+
+/** @type {import('@commitlint/types').UserConfig} */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
